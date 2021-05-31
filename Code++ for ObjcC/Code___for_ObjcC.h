3c1365f456f3a399bb87f891ad8b46af11a423b4
Meterwhite <quxingyi@outlook.com>
2021-05-31 17:05:28 +0800

    Initial Commit

---
+++ Code++ for ObjcC/Code___for_ObjcC.h
@@ -0,0 +1,13 @@
+//___FILEHEADER___
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ___TARGETNAME:c99ExtIdentifier___.
+FOUNDATION_EXPORT double ___TARGETNAME:c99ExtIdentifier___VersionNumber;
+
+//! Project version string for ___TARGETNAME:c99ExtIdentifier___.
+FOUNDATION_EXPORT const unsigned char ___TARGETNAME:c99ExtIdentifier___VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <___TARGETNAME:c99ExtIdentifier___/PublicHeader.h>
+
+
